707a55d610019ccc85cce5ba8811ba4f5831980e
XueYou <xueyoucd@gmail.com>
2019-08-16 17:15:10 +0800

    - 请求缓存

--- src/main/java/com/xueyou/study/servicefeign/ServiceFeignApplication.java
+++ src/main/java/com/xueyou/study/servicefeign/ServiceFeignApplication.java
@@ -2,13 +2,14 @@ package com.xueyou.study.servicefeign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
 @EnableEurekaClient
-@EnableDiscoveryClient
+@SpringCloudApplication
 @EnableFeignClients
 public class ServiceFeignApplication {
 
